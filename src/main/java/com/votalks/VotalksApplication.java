32d7ac53116c34a4e048f5172eac01b1681c0b4a
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-04 16:33:21 +0900

    chore: yaml 파일 설정

---
+++ src/main/java/com/votalks/VotalksApplication.java
@@ -0,0 +1,12 @@
+package com.votalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotalksApplication.class, args);
+	}
+}
