00fbec8cfe6672ab535028342b9acedc3ecb51a7
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-04 11:40:05 +0900

    chore : 프로젝트 셋팅 깃허브 연동 & ignore 파일 추가

---
+++ src/test/java/com/votalks/VotalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
