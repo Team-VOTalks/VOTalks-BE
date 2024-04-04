64f53192a7ca5b881f668fa6734aa3c3a6b386a8
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-04 16:47:21 +0900

    feat: 모든 엔티티 생성
    엔티티 생성 & 연관 관계 매핑

---
+++ src/main/java/com/votalks/api/persistence/Comment.java
@@ -0,0 +1,51 @@
+package com.votalks.api.persistence;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "content", length = 50, nullable = false)
+	private String content;
+
+	@Column(name = "create_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "vote_id")
+	private Vote vote;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "uuid")
+	private Uuid uuid;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "comment_id")
+	private Comment comment;
+
+	@OneToMany(mappedBy = "comment", orphanRemoval = true)
+	private List<Comment> reply = new ArrayList<>();
+}
