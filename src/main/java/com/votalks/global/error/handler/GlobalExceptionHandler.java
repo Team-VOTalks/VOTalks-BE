f6881e010df2c5abab5afc60a992c77535d7ae26
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-08 12:44:02 +0900

    feat: 커스텀 에러 포맷 구현

--- src/main/java/com/votalks/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/votalks/global/error/handler/GlobalExceptionHandler.java
@@ -11,24 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.votalks.global.error.exception.BadRequestException;
 import com.votalks.global.error.exception.NotFoundException;
+import com.votalks.global.error.model.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(NotFoundException.class)
 	protected ResponseEntity<Object> handleNotFoundException(NotFoundException e) {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(BadRequestException.class)
 	protected ResponseEntity<Object> handleValidateException(BadRequestException e) {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
 		List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
 		String errorMessage = fieldErrors.get(0).getDefaultMessage();
-
-		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 }
---
+++ src/main/java/com/votalks/global/error/model/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.votalks.global.error.model;
+
+public record ErrorResponse(String message) {
+}
