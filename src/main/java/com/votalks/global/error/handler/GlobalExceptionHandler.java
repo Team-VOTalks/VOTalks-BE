cba8765b0251fabbf70629cdedaafbeb85b1a9e9
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-08 12:01:34 +0900

    feat: Valid 메시지 추가

--- src/main/java/com/votalks/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/votalks/global/error/handler/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.votalks.global.error.handler;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +22,13 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BadRequestException.class)
 	protected ResponseEntity<Object> handleValidateException(BadRequestException e) {
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+		String errorMessage = fieldErrors.get(0).getDefaultMessage();
+
+		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
 	}
 }
