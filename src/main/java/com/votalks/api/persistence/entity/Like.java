35e25664abeaedc91014cc81b52b7e65ae432ad7
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-09 17:07:19 +0900

    feat: Like 생성 메소드 추가

--- src/main/java/com/votalks/api/persistence/entity/Like.java
+++ src/main/java/com/votalks/api/persistence/entity/Like.java
@@ -1,5 +1,7 @@
 package com.votalks.api.persistence.entity;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,8 +23,19 @@ public class Like {
 	private Long id;
 
 	@Column(name = "like_count", nullable = false)
+	@ColumnDefault("0")
 	private int likeCount;
 
 	@Column(name = "dislike_count", nullable = false)
+	@ColumnDefault("0")
 	private int dislikeCount;
+
+	private Like(int likeCount, int dislikeCount) {
+		this.likeCount = likeCount;
+		this.dislikeCount = dislikeCount;
+	}
+
+	public static Like create() {
+		return new Like(0, 0);
+	}
 }
