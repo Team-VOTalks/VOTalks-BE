786dbb709faaed5fd93cd76ebc650c8c00400ad2
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-11 03:16:46 +0900

    feat: 싫어요 기능

--- src/main/java/com/votalks/api/persistence/repository/UuidLikeRepository.java
+++ src/main/java/com/votalks/api/persistence/repository/UuidLikeRepository.java
@@ -1,7 +1,8 @@
 package com.votalks.api.persistence.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,13 +11,9 @@ import com.votalks.api.persistence.entity.Uuid;
 import com.votalks.api.persistence.entity.UuidLike;
 
 public interface UuidLikeRepository extends JpaRepository<UuidLike, Long> {
-
-	// 존재 여부 확인
 	@Query("SELECT COUNT(ul) > 0 FROM UuidLike ul WHERE ul.uuid = :uuid AND ul.like = :like")
 	boolean existsByUuidAndLike(@Param("uuid") Uuid uuid, @Param("like") Like like);
 
-	// 삭제
-	@Modifying
-	@Query("DELETE FROM UuidLike ul WHERE ul.uuid = :uuid AND ul.like = :like")
-	void deleteByUuidAndLike(@Param("uuid") Uuid uuid, @Param("like") Like like);
+	@Query("SELECT ul FROM UuidLike ul WHERE ul.uuid = :uuid AND ul.like = :like")
+	Optional<UuidLike> findByUuidAndLike(@Param("uuid") Uuid uuid, @Param("like") Like like);
 }
--- src/main/java/com/votalks/api/service/CommentService.java
+++ src/main/java/com/votalks/api/service/CommentService.java
@@ -13,9 +13,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.votalks.api.dto.comment.CommentCreateDto;
+import com.votalks.api.dto.comment.CommentLikeDto;
 import com.votalks.api.dto.comment.CommentReadDto;
 import com.votalks.api.persistence.entity.Comment;
 import com.votalks.api.persistence.entity.Like;
+import com.votalks.api.persistence.entity.LikeType;
 import com.votalks.api.persistence.entity.Uuid;
 import com.votalks.api.persistence.entity.UuidLike;
 import com.votalks.api.persistence.entity.Vote;
@@ -46,12 +48,13 @@ public class CommentService {
 		final Uuid uuid = getOrCreate(dto.uuid());
 		final int userNumber = determineUserNumber(vote, uuid);
 		final Like like = Like.create();
+
 		likeRepository.save(like);
 		commentRepository.save(Comment.create(dto, uuid, vote, like, userNumber));
 	}
 
 	public Page<CommentReadDto> read(Long id, int page, int size) {
-		Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+		final Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
 		final Vote vote = voteRepository.findById(id)
 			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_VOTE_FOUND));
 
@@ -59,32 +62,74 @@ public class CommentService {
 			.map(Comment::toCommentReadDto);
 	}
 
-	public void like(Long voteId, Long commentId, String userUuid) {
+	public void like(Long voteId, Long commentId, CommentLikeDto dto) {
 		final Vote vote = voteRepository.findById(voteId)
 			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_VOTE_FOUND));
 		final Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_COMMENT_FOUND));
 		validateBelongToVote(comment, vote);
-		final Uuid uuid = getOrCreate(userUuid);
+		final Uuid uuid = getOrCreate(dto.uuid());
 		final Like like = comment.getLike();
+		final UuidLike uuidLike = uuidLikeRepository.findByUuidAndLike(uuid, like)
+			.orElse(UuidLike.create(uuid, like));
 
 		if (isAlreadyPress(uuid, like)) {
-			cancelLike(uuid, like);
+			validateCancelLike(like, dto.likeType(), uuidLike);
+			validateChangeLike(like, dto.likeType(), uuidLike);
 			return;
 		}
 
-		final UuidLike uuidLike = UuidLike.create(uuid, like);
-		like.pressLike();
+		validatePressLike(dto, like, uuidLike);
+		validatePressDislike(dto, like, uuidLike);
 		uuidLikeRepository.save(uuidLike);
+	}
+
+	private void validatePressDislike(CommentLikeDto dto, Like like, UuidLike uuidLike) {
+		if (dto.likeType().equals(DISLIKE)) {
+			like.pressDisLike();
+			uuidLike.dislikeType();
+		}
+	}
+
+	private void validatePressLike(CommentLikeDto dto, Like like, UuidLike uuidLike) {
+		if (dto.likeType().equals(LIKE)) {
+			like.pressLike();
+			uuidLike.likeType();
+		}
 	}
 
 	private boolean isAlreadyPress(Uuid uuid, Like like) {
 		return uuidLikeRepository.existsByUuidAndLike(uuid, like);
 	}
 
-	private void cancelLike(Uuid uuid, Like like) {
-		like.cancelLike();
-		uuidLikeRepository.deleteByUuidAndLike(uuid, like);
+	private void validateCancelLike(Like like, String likeType, UuidLike uuidLike) {
+		if (likeType.equals(LIKE) && uuidLike.getLikeType().equals(LikeType.LIKE)) {
+			like.cancelLike();
+			uuidLikeRepository.delete(uuidLike);
+		}
+		
+		if (likeType.equals(DISLIKE) && uuidLike.getLikeType().equals(LikeType.DISLIKE)) {
+			like.cancelDislike();
+			uuidLikeRepository.delete(uuidLike);
+		}
+	}
+
+	/**
+	 * 이미 싫어요를 눌렀는데, 좋아요를 누르는 경우
+	 * 이미 좋아요를 눌렀는데, 싫어요를 누르는 경우
+	 */
+	private void validateChangeLike(Like like, String likeType, UuidLike uuidLike) {
+		if (uuidLike.getLikeType().equals(LikeType.DISLIKE) && likeType.equals(LIKE)) {
+			like.cancelDislike();
+			like.pressLike();
+			uuidLike.likeType();
+		}
+
+		if (uuidLike.getLikeType().equals(LikeType.LIKE) && likeType.equals(DISLIKE)) {
+			like.cancelLike();
+			like.pressDisLike();
+			uuidLike.dislikeType();
+		}
 	}
 
 	private static void validateBelongToVote(Comment comment, Vote vote) {
