3a74ec2fcad9ee16232712f8844faeca39b9cfec
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-11 14:20:46 +0900

    refactor: 댓글 서비스 분리

--- src/main/java/com/votalks/api/controller/CommentController.java
+++ src/main/java/com/votalks/api/controller/CommentController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.votalks.api.dto.comment.CommentCreateDto;
-import com.votalks.api.dto.comment.CommentLikeDto;
 import com.votalks.api.dto.comment.CommentReadDto;
 import com.votalks.api.service.CommentService;
 
@@ -18,30 +17,21 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/comments/{id}")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class CommentController {
 	private final CommentService commentService;
 
-	@PostMapping
-	public void create(@RequestBody @Valid CommentCreateDto dto, @PathVariable Long id) {
+	@PostMapping("/vote/{vote-id}/comment")
+	public void create(@RequestBody @Valid CommentCreateDto dto, @PathVariable(name = "vote-id") Long id) {
 		commentService.create(dto, id);
 	}
 
-	@PostMapping("/comment/{comment-id}/like")
-	public void like(
-		@PathVariable Long id,
-		@PathVariable(name = "comment-id") Long commentId,
-		@RequestBody CommentLikeDto dto
-	) {
-		commentService.like(id, commentId, dto);
-	}
-
-	@GetMapping
+	@GetMapping("/vote/{vote-id}/comment")
 	public Page<CommentReadDto> read(
 		@RequestParam(defaultValue = "0") int page,
 		@RequestParam(defaultValue = "10") int size,
-		@PathVariable Long id) {
+		@PathVariable(name = "vote-id") Long id) {
 		return commentService.read(id, page, size);
 	}
 }
--- src/main/java/com/votalks/api/service/CommentService.java
+++ src/main/java/com/votalks/api/service/CommentService.java
@@ -13,17 +13,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.votalks.api.dto.comment.CommentCreateDto;
-import com.votalks.api.dto.comment.CommentLikeDto;
 import com.votalks.api.dto.comment.CommentReadDto;
 import com.votalks.api.persistence.entity.Comment;
 import com.votalks.api.persistence.entity.Like;
-import com.votalks.api.persistence.entity.LikeType;
 import com.votalks.api.persistence.entity.Uuid;
-import com.votalks.api.persistence.entity.UuidLike;
 import com.votalks.api.persistence.entity.Vote;
 import com.votalks.api.persistence.repository.CommentRepository;
 import com.votalks.api.persistence.repository.LikeRepository;
-import com.votalks.api.persistence.repository.UuidLikeRepository;
 import com.votalks.api.persistence.repository.UuidRepository;
 import com.votalks.api.persistence.repository.VoteRepository;
 import com.votalks.global.error.exception.NotFoundException;
@@ -39,7 +35,6 @@ public class CommentService {
 	private final VoteRepository voteRepository;
 	private final UuidRepository uuidRepository;
 	private final LikeRepository likeRepository;
-	private final UuidLikeRepository uuidLikeRepository;
 
 	public void create(CommentCreateDto dto, Long id) {
 		final Vote vote = voteRepository.findById(id)
@@ -60,49 +55,6 @@ public class CommentService {
 
 		return commentRepository.findAllByVote(vote, pageable)
 			.map(Comment::toCommentReadDto);
-	}
-
-	public void like(Long voteId, Long commentId, CommentLikeDto dto) {
-		final Comment comment = commentRepository.findByIdAndVote_Id(commentId, voteId)
-			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_COMMENT_FOUND));
-		final Uuid uuid = getOrCreate(dto.uuid());
-		final Like like = comment.getLike();
-		final LikeType likeType = LikeType.from(dto.likeType());
-
-		uuidLikeRepository.findByUuidAndLike(uuid, like)
-			.ifPresentOrElse(
-				uuidLike -> validateCancelLike(like, likeType, uuidLike),
-				() -> {
-					UuidLike uuidLike = UuidLike.create(uuid, like, likeType);
-					uuidLikeRepository.save(uuidLike);
-				});
-	}
-
-	private void validateCancelLike(Like like, LikeType likeType, UuidLike uuidLike) {
-		if (likeType.isLike() && uuidLike.getLikeType().isLike()) {
-			like.cancelLike();
-			uuidLikeRepository.delete(uuidLike);
-			return;
-		}
-
-		if (likeType.isDislike() && uuidLike.getLikeType().isDislike()) {
-			like.cancelDislike();
-			uuidLikeRepository.delete(uuidLike);
-			return;
-		}
-
-		if (uuidLike.getLikeType().isDislike() && likeType.isLike()) {
-			like.cancelDislike();
-			like.pressLike();
-			uuidLike.likeType();
-			return;
-		}
-
-		if (uuidLike.getLikeType().isLike() && likeType.isDislike()) {
-			like.cancelLike();
-			like.pressDisLike();
-			uuidLike.dislikeType();
-		}
 	}
 
 	private int determineUserNumber(Vote vote, Uuid uuid) {
