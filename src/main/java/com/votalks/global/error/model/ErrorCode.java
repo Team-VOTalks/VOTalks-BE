ceccc6e138b198aea4b16440e25d61d210251884
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-15 02:58:13 +0900

    feat: 답글 좋아요 기능

--- src/main/java/com/votalks/global/error/model/ErrorCode.java
+++ src/main/java/com/votalks/global/error/model/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
 	FAIL_NOT_VOTE_OPTION_FOUND("해당 투표 내용을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 	FAIL_NOT_VOTE_FOUND("투표를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 	FAIL_NOT_COMMENT_FOUND("댓글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+	FAIL_NOT_REPLY_FOUND("답글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 	BAD_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
 	VOTE_LIMIT_REACHED("투표 한도에 도달했습니다", HttpStatus.BAD_REQUEST),
 	FAIL_ALREADY_VOTE("이미 투표를 하셨습니다.", HttpStatus.BAD_REQUEST);
